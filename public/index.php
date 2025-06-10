53e7890cdd79ede3698bd8b33869f58943a8c9e5
Guilherme Gemniczak da Rocha <guilherme>
2025-06-10 02:21:31 -0300

    estilo: estilo do index inicial

--- public/index.php
+++ public/index.php
@@ -1,4 +1,29 @@
-<?php
-require_once __DIR__ . '/../src/Controllers/UserController.php';
-echo findUserByEmail('lovesongs2@gmail.com')
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nome do jogo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="conteiner">
+    <div class="main">
+
+    <div class="titulo">
+        <h1>Typos!</h1>
+    </div>
+    <div class="botao-wrapper">
+        <a href="register.php" class="botao">Sign up</a>
+        <a href="login.php" class="botao">Sign in</a>
+    </div>
+
+    <p>(Press "esc" to use the command bar, type :help, for adicional comands)</p>
+    </div>
+    </div>
+
+
+    <div class="crt-overlay"></div>
+    <div class="frame"></div>
+</body>
+</html>
