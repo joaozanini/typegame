13ca9feb6ce8feb7adb77ada4953b2254c150ad2
Guilherme Gemniczak da Rocha <guilherme>
2025-06-16 17:26:51 -0300

    Manutenção: css limpo e mais controlável, com mais acessibilidade de texto

--- public/profile.php
+++ public/profile.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/profile.css">
 </head>
 <body>
