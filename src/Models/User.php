1c52c85b26d2ba5f8a379f989f03297c9d4abda3
João Vitor <joaozanini.profissional@gmail.com>
2025-06-16 21:23:27 -0300

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -140,6 +140,8 @@ function findPublicUserByUsername($user) {
     return $user;
 }
 
+// GAME UTILS
+
 function addPoints($userId, $points) {
     $conn = db_connect();
     $stmt = $conn->prepare("UPDATE user SET total_points = total_points + ? WHERE id = ?");
@@ -150,10 +152,22 @@ function addPoints($userId, $points) {
     return $success;
 }
 
+// LEAGUE UTILS
+
 function joinLeague($leagueId, $userId) {
     $conn = db_connect();
     $stmt = $conn->prepare("INSERT INTO user (league_id) VALUES (?) WHERE id = ?");
     $stmt->bind_param("ii", $leagueId, $userId);
+    $success = $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    return $success;
+}
+
+function leaveLeague($userId) {
+    $conn = db_connect();
+    $stmt = $conn->prepare("UPDATE user SET league_id = NULL WHERE id = ?");
+    $stmt->bind_param("i", $userId);
     $success = $stmt->execute();
     $stmt->close();
     $conn->close();
