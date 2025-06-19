2597c7710f62cebefa6a94f1035380f10f62d8a5
Guilherme Gemniczak da Rocha <guilherme>
2025-06-19 02:41:26 -0300

    Style: User com HTML table

--- public/profile.php
+++ public/profile.php
@@ -20,7 +20,10 @@ if (!isset($_SESSION['user_id'])) {
     <div class="main">
         <div id="userInfo">
             <img src="img/placeholder.png" alt="">
-            <h1 id="username">PLACEHOLDER</h1>
+            <div id="userData">
+                <h1 id="username">PLACEHOLDER</h1>
+                <h2>Total score: 111111111111111</h2>
+            </div>
             <div class="buttonWrapper">
                 <input type="button" class="botao" value="Logout">
                 <input type="button" class="botao" value="Edit">
@@ -31,32 +34,67 @@ if (!isset($_SESSION['user_id'])) {
 
         <div id="userHistory">
             <h2>History:</h2>
-            <section>
-                <div class="historyUnit">
-                    <div class="matchHeader">
-                        <h3>Match #1</h3>
-                        <h3>Date: 2025-3-3</h3>
-                    </div>
-                    <p>WPM: 100</p>
-                    <p>Errors: </p>
-                    <p>Accuracy: </p>
-                    <p>Time: 30s</p>
-                    <h4>Total score: 100</h4>
-                </div>
-
-                <div class="historyUnit">
-                    <div class="matchHeader">
-                        <h3>Match #1</h3>
-                        <h3>Date: 2025-3-3</h3>
-                    </div>
-                    <p>WPM: 100</p>
-                    <p>Errors: </p>
-                    <p>Accuracy: </p>
-                    <p>Time: 30s</p>
-                    <h4>Total score: 100</h4>
-                </div>
-
-
+            <section id="historyConteiner">
+                <table class="historyTable">
+                    <thead>
+                        <tr>
+                            <th>Match</th>
+                            <th>Date</th>
+                            <th>WPM</th>
+                            <th>Errors</th>
+                            <th>Accuracy</th>
+                            <th>Time</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#1</td>
+                            <td>2025-3-3</td>
+                            <td>100</td>
+                            <td>—</td>
+                            <td>—</td>
+                            <td>30s</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>#2</td>
+                            <td>2025-3-3</td>
+                            <td>100</td>
+                            <td>—</td>
+                            <td>—</td>
+                            <td>30s</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>#3</td>
+                            <td>2025-3-3</td>
+                            <td>100</td>
+                            <td>—</td>
+                            <td>—</td>
+                            <td>30s</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>#4</td>
+                            <td>2025-3-3</td>
+                            <td>100</td>
+                            <td>—</td>
+                            <td>—</td>
+                            <td>30s</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>#5</td>
+                            <td>2025-3-3</td>
+                            <td>100</td>
+                            <td>—</td>
+                            <td>—</td>
+                            <td>30s</td>
+                            <td>100</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
 
         </div>
